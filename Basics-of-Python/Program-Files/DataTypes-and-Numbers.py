45abe7609fde9dd631531a9ca58b63138ae0eacf
Aayush Aarav <aarav.aayush14@gmail.com>
2026-07-29 18:08:46 +0530

    Updated Lesson on DataTypes-and-Numbers.

---
+++ Basics-of-Python/Program-Files/DataTypes-and-Numbers.py
@@ -0,0 +1,31 @@
+# Talking about data-types
+'''
+Lots of data types exist in Python:
+Text based: str
+Numeric based: int, float, complex
+Sequence based: list, tuple, range
+Mapping based: dict
+Set based: set, frozenset
+True/False based: bool
+Binary based: bytes, bytearray, memoryview
+None based: noneType
+'''
+
+# Printing all types of Numbers and Type-Casting
+'''
+x=5
+y=3.14
+z=9+5j
+print(x)
+print(y)
+print(z)
+print(complex(x))
+print(int(y))
+print(complex(y))
+print(float(x))
+print()
+'''
+
+# Using random module
+import random
+print(random.randrange(1,10)) # Prints random integers from [1,10)
