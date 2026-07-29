1e08904877f963572964520e563acae4c5d7f0c2
Aayush Aarav <aarav.aayush14@gmail.com>
2026-07-29 17:02:24 +0530

    Completed Chapter on Printing in python!

---
+++ Basics-of-Python/Printing.py
@@ -0,0 +1,29 @@
+# Basic Printing and calculations (This is a comment btw)
+print("Hello World")
+print (5+6)
+print(7-3)
+print(7**2)
+print(7*3)
+print(5>6)
+
+# Use of indentation
+if(5>6):
+    print("My name is Chotu")
+else:
+    print("My name is Aayush")
+
+# Variable name assignment 
+x=6
+y="Chotu is a loser"
+print(x,y)
+
+# Print multiple lines/words in 1 line (end parameter)
+print("Aayush Aarav", end=" ")
+print("Raj Kumar")
+
+# Use of multi-line comments
+'''
+This is a multi-line comment
+You can read this very well
+Using single-line comments makes it harder to read
+'''
