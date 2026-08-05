1e346a508bd9ebbb65993ee85e46f62bc1795c58
Aayush Aarav <aarav.aayush14@gmail.com>
2026-08-05 21:46:56 +0530

    Finished lesson on Modules!

--- Basics-of-Python/Program-Files/Boring_Stuff.py
+++ Basics-of-Python/Program-Files/Boring_Stuff.py
@@ -7,3 +7,27 @@ print(list(x[0:3]))
 print(len(x))
 print(7 in x)
 '''
+
+# Treat lists as 'ARRAYS' unless you use NumPy, etc
+
+# Iterators (Just Basic, advanced stuff after learning about classes)
+'''
+myTuple=('Apple','Banana','Orange')
+myIterator=iter(myTuple)
+print(next(myIterator))
+print(next(myIterator))
+print(next(myIterator))
+'''
+
+# Using Modules:
+'''
+import Test_Module as mod
+directory=dir(mod) # Lists out all the variable names in the module
+print(directory)
+'''
+
+# Importing using 'from' keyword
+'''
+from Test_Module import testModule as tM
+tM()
+'''
---
+++ Basics-of-Python/Program-Files/Test_Module.py
@@ -0,0 +1,6 @@
+def testModule():
+    print('Hi!')
+def testModule2():
+    print('Bye!')
+a=10
+b=15
